47d891d8da458ed8be081b4a70f345eb1482229f
Harua Izawa <ec1167xx@ed.ritsumei.ac.jp>
2025-09-16 14:47:05 +0900

    Update models.py

--- chat_choice/models.py
+++ chat_choice/models.py
@@ -12,9 +12,14 @@ class C(BaseConstants):
 
 class Subsession(BaseSubsession):
     def creating_session(self):
-        if self.round_number <= 7:
+        if self.round_number == 1:
+            # 1ラウンド目だけランダムにして、それを固定
             self.group_randomly(fixed_id_in_group=True)
+        elif self.round_number <= 7:
+            # 2〜7ラウンド目は1ラウンド目と同じグループを使う
+            self.group_like_round(1)
         else:
+            # 8ラウンド目以降は完全ランダム
             self.group_randomly()
 
 
@@ -168,6 +173,7 @@ def check_timeout_and_missing_q(group: Group, **kwargs):
         if p.timed_out and p.q == 0:
             group.force_terminate = True
             break
+
 
 
 
